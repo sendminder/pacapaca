a523eac4922067d48cd0985dde8e6a58e12d975c
sendminder <sendminder@gmail.com>
2025-02-02 19:39:40 +0900

    hello world

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MyApp extends ConsumerStatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello, World!'),
+      ),
+    );
+  }
+}
